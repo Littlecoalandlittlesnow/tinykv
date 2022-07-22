62e0c2c3323e891d963f5121bc0ff685be2a130d
fantastic <58289931+Littlecoalandlittlesnow@users.noreply.github.com>
2022-07-22 14:20:21 +0800

    Add files via upload

---
+++ kv/test_raftstore/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for ((i=$1;i<=$2;i++))
+do
+#  go test -test.v -test.run="TestBasic2B" >> TestBasic2B.log
+#  go test -test.v -test.run="TestConcurrent2B" >> TestConcurrent2B.log
+#  go test -test.v -test.run="TestUnreliable2B" >> TestUnreliable2B.log
+#  go test -test.v -test.run="TestOnePartition2B" >> TestOnePartition2B.log
+#  go test -test.v -test.run="TestManyPartitionsOneClient2B" >> TestManyPartitionsOneClient2B.log
+#  go test -test.v -test.run="TestManyPartitionsManyClients2B" >> TestManyPartitionsManyClients2B.log
+#  go test -test.v -test.run="TestPersistOneClient2B" >> TestPersistOneClient2B.log
+#  go test -test.v -test.run="TestPersistConcurrent2B" >> TestPersistConcurrent2B.log
+#  go test -test.v -test.run="TestPersistConcurrentUnreliable2B" >> TestPersistConcurrentUnreliable2B.log
+#  go test -test.v -test.run="TestPersistPartition2B" >> TestPersistPartition2B.log
+#  go test -test.v -test.run="TestPersistPartitionUnreliable2B" >> 2B.log
+#   go test -test.v -test.run="TestOneSnapshot2C" > 2C"$i".log
+  go test -test.v -test.run="TestSnapshotUnreliableRecoverConcurrentPartition2C" > 2C"$i".log
+done
